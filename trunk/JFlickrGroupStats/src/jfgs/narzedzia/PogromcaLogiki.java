5bd07df5e291f34f95d814f0b115223cade2b3d0
michalus <michalus@3ee77afc-956b-11dd-acb2-99339305f672>
2009-08-31 21:37:46 +0000

    Licencja

--- trunk/JFlickrGroupStats/src/jfgs/narzedzia/PogromcaLogiki.java
+++ trunk/JFlickrGroupStats/src/jfgs/narzedzia/PogromcaLogiki.java
@@ -1,11 +1,25 @@
 /*
+ *  This file is part of JFlickrGroupStats.
+ *
+ *  JFlickrGroupStats is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JFlickrGroupStats is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with JFlickrGroupStats.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package jfgs.narzedzia;
 
 import javax.swing.ComboBoxModel;
 import javax.swing.DefaultComboBoxModel;
-import javax.swing.JPanel;
 import jfgs.ak.AnalizaKomentarzy;
 import jfgs.gui.KontrolerGUI;
 import jfgs.zm.ZdjecieMiesiaca;
