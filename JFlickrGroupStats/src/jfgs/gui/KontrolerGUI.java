de58fe02daa6df975c59f7ac351de97319a0fd66
michalus <michalus@3ee77afc-956b-11dd-acb2-99339305f672>
2009-08-31 21:37:46 +0000

    Licencja

--- JFlickrGroupStats/src/jfgs/gui/KontrolerGUI.java
+++ JFlickrGroupStats/src/jfgs/gui/KontrolerGUI.java
@@ -1,6 +1,19 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  This file is part of JFlickrGroupStats.
+ *
+ *  JFlickrGroupStats is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JFlickrGroupStats is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with JFlickrGroupStats.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package jfgs.gui;
@@ -15,7 +28,6 @@ import java.text.ParseException;
 import java.util.Date;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 import javax.xml.parsers.ParserConfigurationException;
